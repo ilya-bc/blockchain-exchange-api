7d5ab35511ebc2e571afd70263c1d5ed9340286d
Ilya Kisil <ilya@blockchain.com>
2020-05-17 23:30:31 +0100

    docs: make sphinx-gallery examples user friendly

--- examples/run-00-subscribe-to-public-channels.py
+++ examples/run-00-subscribe-to-public-channels.py
@@ -7,44 +7,59 @@ Subscribe to Public channels
     :local:
     :depth: 1
 """
+###########################################################################
+# Initialise client
+# =================
+# By default all incoming messages are being logged at INFO level without
+# any modifications. So you would want to set logging accordingly in case
+# you need to visually inspect results.
 import time
 import logging
 from pprint import pprint
 
 from bcx.client import BlockchainWebsocketClient
 
-
 logging.basicConfig(level=logging.INFO)
 
-
-def main():
-    client = BlockchainWebsocketClient()
-    client.subscribe_to_heartbeat()
-    client.subscribe_to_prices("BTC-USD", granularity=60)
-    client.subscribe_to_prices("ETH-USD", granularity=60)
-    client.subscribe_to_ticker("BTC-USD")
-    client.subscribe_to_trades("BTC-USD")
-    time.sleep(2)
-    pprint(client.connected_channels)
-
-    time.sleep(2)
-    client.subscribe_to_symbols()
-    time.sleep(2)
-    pprint(client.connected_channels)
-
-    time.sleep(2)
-    client.subscribe_to_orderbook_l2("BTC-USD")
-    time.sleep(2)
-    pprint(client.connected_channels)
-
-    time.sleep(2)
-    client.subscribe_to_orderbook_l3("BTC-USD")
-    time.sleep(2)
-    pprint(client.connected_channels)
-
-    time.sleep(7)
-    print(f"Last Heart Beat: {client.get_last_heartbeat()}")
+client = BlockchainWebsocketClient()
 
 
-if __name__ == '__main__':
-    main()
+###########################################################################
+# Subscribe for monitoring
+# ========================
+# Subscription can happen on a fly, i.e. you don't need to restart client
+# (or re-run script) to be able to subscribe to new channels.
+client.subscribe_to_heartbeat()
+
+client.subscribe_to_prices("BTC-USD", granularity=60)
+
+client.subscribe_to_prices("ETH-USD", granularity=60)
+
+client.subscribe_to_ticker("BTC-USD")
+
+client.subscribe_to_trades("BTC-USD")
+
+time.sleep(2)
+pprint(client.connected_channels)
+
+
+###########################################################################
+# Some channels send a snapshot of when you connect to them for the very
+# first time.
+time.sleep(2)
+client.subscribe_to_symbols()
+
+client.subscribe_to_orderbook_l2("BTC-USD")
+
+client.subscribe_to_orderbook_l3("BTC-USD")
+
+time.sleep(2)
+pprint(client.connected_channels)
+
+
+###########################################################################
+# This is a simple use case showing that the client not only reads incoming
+# messages and logs them, but also can parse them and store the result in
+# memory in order to be used later on, i.e. by external API.
+time.sleep(7)
+print(f"Last Heart Beat: {client.get_last_heartbeat()}")
--- examples/run-01-subscribe-to-trading-channel.py
+++ examples/run-01-subscribe-to-trading-channel.py
@@ -7,56 +7,89 @@ Subscribe to Trading Channel
     :local:
     :depth: 1
 """
+###########################################################################
+# Initialise client
+# =================
+# By default all incoming messages are being logged at INFO level without
+# any modifications. So you would want to set logging accordingly in case
+# you need to visually inspect results.
 import time
 import logging
 from pprint import pprint
 
 from bcx.client import BlockchainWebsocketClient
 
-
 logging.basicConfig(level=logging.INFO)
 
-
-def main():
-    client = BlockchainWebsocketClient()
-
-    client.subscribe_to_balances()
-    time.sleep(2)
-
-    pprint(client.connected_channels)
-    time.sleep(2)
-
-    client.subscribe_to_trading()
-    time.sleep(2)
-
-    pprint(client.connected_channels)
-    time.sleep(2)
-
-    print("Submit Market Order")
-    time.sleep(2)
-    client.create_market_order(
-        order_id="my-order",
-        symbol="BTC-USD",
-        time_in_force="GTC",
-        side="sell",
-        quantity=0.000000000001,
-    )
-    time.sleep(2)
-
-    print("Submit Limit Order")
-    time.sleep(2)
-    client.create_limit_order(
-        order_id="my-order",
-        price=100000000000000.0,
-        symbol="BTC-USD",
-        time_in_force="GTC",
-        side="sell",
-        quantity=0.000000000001,
-    )
-
-    time.sleep(10)
-    client.cancel_all_orders()
+client = BlockchainWebsocketClient()
 
 
-if __name__ == '__main__':
-    main()
+###########################################################################
+# Subscribe for trading
+# =====================
+# Actual trading and accessing balance of your account `requires authentication
+# <https://exchange.blockchain.com/api/#authenticated-channels>`_ with an API key.
+# All you need to do is to make sure that ``BLOCKCHAIN_API_SECRET`` environment
+# variable is present at script run time.
+#
+# .. code-block:: shell
+#
+#   export BLOCKCHAIN_API_SECRET="__ENTER_YOUR_API_SECRET_HERE__"
+#
+# The client will handle the rest of the authentication process
+client.subscribe_to_balances()
+time.sleep(2)
+
+client.subscribe_to_trading()
+time.sleep(2)
+
+pprint(client.connected_channels)
+time.sleep(2)
+
+
+###########################################################################
+# Create market order
+# ===================
+# Market order is an order that will match at any price available in the
+# market, starting from the best prices and filling up to the available
+# balance.
+#
+# .. important ::
+#   At the moment, there is no logic in place which handles responses from
+#   the server when you create an order
+time.sleep(2)
+client.create_market_order(
+    order_id="my-order",
+    symbol="BTC-USD",
+    time_in_force="GTC",
+    side="sell",
+    quantity=0.000000000001,
+)
+
+
+###########################################################################
+# Create limit order
+# ===================
+# Limit order is an order that has a price limit.
+#
+# .. important ::
+#   At the moment, there is no logic in place which handles responses from
+#   the server when you create an order
+time.sleep(2)
+client.create_limit_order(
+    order_id="my-order",
+    price=100000000000000.0,
+    symbol="BTC-USD",
+    time_in_force="GTC",
+    side="sell",
+    quantity=0.000000000001,
+)
+
+
+###########################################################################
+# Cancel all orders
+# =================
+# The client stores all necessary information about placed orders. This can
+# be used to track order execution status or cancel all of the in one go
+time.sleep(2)
+client.cancel_all_orders()
